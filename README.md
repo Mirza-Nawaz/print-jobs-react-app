07e251dca82026b5b5ec73d41dcc771c43c34c95
Mirza-Nawaz <nawazbaig0610@gmail.com>
2024-11-09 00:20:42 +0530

    Added Light Mode

--- README.md
+++ README.md
@@ -1,8 +1,38 @@
-# React + Vite
+# Print-Jobs-React-App
 
-This template provides a minimal setup to get React working in Vite with HMR and some ESLint rules.
+I developed a sleek and modern application using React with Vite as the bundler, incorporating the Ant Design library for a clean and professional interface. To elevate the user experience, I customized Ant Design with a glassmorphism aesthetic, giving the UI a translucent, frosted glass effect that adds depth and elegance to the visuals. The application currently operates with dummy data, providing a responsive and visually appealing layout that showcases the design potential and functionality of the interface.
 
-Currently, two official plugins are available:
+## Getting Started
 
-- [@vitejs/plugin-react](https://github.com/vitejs/vite-plugin-react/blob/main/packages/plugin-react/README.md) uses [Babel](https://babeljs.io/) for Fast Refresh
-- [@vitejs/plugin-react-swc](https://github.com/vitejs/vite-plugin-react-swc) uses [SWC](https://swc.rs/) for Fast Refresh
+To run the development server:
+
+```bash
+npm install
+npm run dev
+# or
+yarn install
+yarn run dev
+```
+
+your application will be started on the following address:
+[localhost](http://localhost:5173/)
+## Responsive Design
+  ### Desktop View
+  ![image](https://github.com/user-attachments/assets/968cf093-f22e-4f3d-b419-f1e301095f5a)
+  ### Mobile View
+  ![image](https://github.com/user-attachments/assets/849a3cb6-3bd1-49d6-8cb6-66c51e7efdb9)
+
+## Print Jobs for Admin
+![image](https://github.com/user-attachments/assets/f9acc515-31ff-4623-87f9-e5de622f748d)
+  ### Job Filters for admin
+  ![image](https://github.com/user-attachments/assets/664729a5-688c-4a7c-a9b4-a2ea2eb43c98)
+  ### Queued Jobs
+  ![image](https://github.com/user-attachments/assets/7e760588-da99-469c-804f-0bbb5ed5991c)
+  ### In Progress Jobs
+  ![image](https://github.com/user-attachments/assets/b4784cdb-1d93-4f22-9319-4c302325335e)
+  ### Compleleted Jobs
+  ![image](https://github.com/user-attachments/assets/e89981f7-792e-4c77-8497-01bf772e4808)
+
+
+## Print Jobs for Technician
+![image](https://github.com/user-attachments/assets/eb4c5350-e8e6-475a-ae71-d76e0936d153)
--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import "./App.css";
@@ -16,56 +16,62 @@ const data = [
     title: "Print Job 1",
     id: "1234567890",
     status: "Queued",
-    icon: <LoadingOutlined style={{ color: "white" }} />,
     statusId: 1,
   },
   {
     title: "Print Job 2",
     id: "1234567890",
     status: "Completed",
-    icon: <CheckCircleOutlined style={{ color: "green" }} />,
     statusId: 3,
   },
   {
     title: "Print Job 3",
     id: "1234567890",
     status: "Completed",
-    icon: <CheckCircleOutlined style={{ color: "green" }} />,
     statusId: 3,
   },
   {
     title: "Print Job 4",
     id: "1234567890",
     status: "In Progress",
-    icon: <Loading3QuartersOutlined style={{ color: "white" }} />,
     statusId: 2,
   },
   {
     title: "Print Job 5",
     id: "1234567890",
     status: "In Progress",
-    icon: <Loading3QuartersOutlined style={{ color: "white" }} />,
     statusId: 2,
   },
   {
     title: "Print Job 6",
     id: "1234567890",
     status: "In Progress",
-    icon: <Loading3QuartersOutlined style={{ color: "white" }} />,
     statusId: 2,
   },
   {
     title: "Print Job 7",
     id: "1234567890",
     status: "Queued",
-    icon: <LoadingOutlined style={{ color: "white" }} />,
     statusId: 1,
   },
 ];
 
 function App() {
+  const [theme, setTheme] = useState();
   const [isAdmin, setIsAdmin] = useState(true);
   const [filteredData, setFilteredData] = useState(data);
+
+  useEffect(() => {
+    const updateTheme = (e) => {
+      setTheme(e.matches ? "dark" : "light");
+    };
+
+    const mediaQuery = window.matchMedia("(prefers-color-scheme: dark)");
+    setTheme(mediaQuery.matches ? "dark" : "light");
+    mediaQuery.addEventListener("change", updateTheme);
+
+    return () => mediaQuery.removeEventListener("change", updateTheme);
+  }, []);
 
   const handleRoleChange = (value) => {
     if (value === 1) {
@@ -94,113 +100,146 @@ function App() {
       theme={{
         token: {
           colorTextPlaceholder: "#a2a2a2",
-          colorText:"white",
-          colorBgBase:"#000000"
+          colorText: (theme === "dark" && "white") || "black",
+          colorBgBase: (theme === "dark" && "#000000") || "white",
         },
-        components:{
-          Select:{
-            selectorBg:"ffffff66",
-            colorBorder:"6b8e8f",
-            optionSelectedBg:"rgb(53 53 53)",
-            optionActiveBg:"#00000000",
-            colorIcon:"white"
-          }
-        }
+        components: {
+          Select: {
+            selectorBg: "ffffff66",
+            colorBorder: "6b8e8f",
+            optionSelectedBg: (theme === "dark" && "rgb(53 53 53)") || "blue",
+            optionSelectedColor: "white",
+            optionActiveBg: "#00000000",
+            colorIcon: "white",
+          },
+        },
       }}
     >
-    <Card
-      className="glass"
-      style={{
-        width: "95vw",
-        margin: "5% auto",
-      }}
-    >
-      <Title level={2}>Print Jobs ( {filteredData.length} )</Title>
-      <Select
-        selec
-        showSearch
-        defaultValue={1}
-        placeholder="Select a Role"
-        optionFilterProp="label"
-        onChange={handleRoleChange}
-        options={[
-          {
-            value: 1,
-            label: "Admin",
-          },
-          {
-            value: 2,
-            label: "Technician",
-          },
-        ]}
+      <Card
+        className="glass"
         style={{
-          marginBottom: "20px",
+          width: "95vw",
+          margin: "5% auto",
         }}
-      />
-      {isAdmin && (
+      >
+        <Title level={2}>Print Jobs ( {filteredData.length} )</Title>
         <Select
+          selec
           showSearch
-          placeholder="Select a Job Status"
+          defaultValue={1}
+          placeholder="Select a Role"
           optionFilterProp="label"
-          onChange={handleFilterChange}
+          onChange={handleRoleChange}
           options={[
             {
               value: 1,
-              label: "Queued",
+              label: "Admin",
             },
             {
               value: 2,
-              label: "In Progress",
-            },
-            {
-              value: 3,
-              label: "Completed",
-            },
-            {
-              value: 4,
-              label: "All",
+              label: "Technician",
             },
           ]}
           style={{
             marginBottom: "20px",
-            marginLeft: "5px",
           }}
         />
-      )}
-      <List
-        itemLayout="horizontal"
-        dataSource={filteredData}
-        renderItem={(item, index) => (
-          <List.Item
+        {isAdmin && (
+          <Select
+            showSearch
+            placeholder="Select a Job Status"
+            optionFilterProp="label"
+            onChange={handleFilterChange}
+            options={[
+              {
+                value: 1,
+                label: "Queued",
+              },
+              {
+                value: 2,
+                label: "In Progress",
+              },
+              {
+                value: 3,
+                label: "Completed",
+              },
+              {
+                value: 4,
+                label: "All",
+              },
+            ]}
             style={{
-              padding: "20px",
-              width: "85vw",
-              borderRadius: "5px",
-              marginBottom: "10px",
-              transition: "box-shadow 0.3s ease",
+              marginBottom: "20px",
+              marginLeft: "5px",
             }}
-            onMouseEnter={(e) => {
-              e.currentTarget.style.boxShadow =
-                "0px 0px 5px rgb(189 189 196 / 50%)";
-            }}
-            onMouseLeave={(e) => {
-              e.currentTarget.style.boxShadow = "none";
-            }}
-          >
-            <List.Item.Meta
-              avatar={item.icon}
-              title={item.title}
-              description={
-                <div>
-                  <p style={{ margin: "0 auto", color: "white" }}>{item.id}</p>
-                  <p style={{ margin: "0 auto", color: "#b2aeae" }}>{item.status}</p>
-                </div>
-              }
-            />
-          </List.Item>
+          />
         )}
-      />
-    </Card>
+        <List
+          itemLayout="horizontal"
+          dataSource={filteredData}
+          renderItem={(item, index) => (
+            <List.Item
+              style={{
+                padding: "20px",
+                width: "85vw",
+                borderRadius: "5px",
+                marginBottom: "10px",
+                transition: "box-shadow 0.3s ease",
+              }}
+              onMouseEnter={(e) => {
+                e.currentTarget.style.boxShadow =
+                  (theme === "dark" && "0px 0px 5px rgb(189 189 196 / 50%)") ||
+                  "0px 0px 5px rgb(0 22 255 / 50%)";
+              }}
+              onMouseLeave={(e) => {
+                e.currentTarget.style.boxShadow = "none";
+              }}
+            >
+              <List.Item.Meta
+                avatar={
+                  (item.statusId === 1 && (
+                    <LoadingOutlined
+                      style={
+                        (theme === "dark" && { color: "white" }) || {
+                          color: "blue",
+                        }
+                      }
+                    />
+                  )) ||
+                  (item.statusId === 2 && (
+                    <Loading3QuartersOutlined
+                      style={
+                        (theme === "dark" && { color: "white" }) || {
+                          color: "blue",
+                        }
+                      }
+                    />
+                  )) ||
+                  (item.statusId === 3 && (
+                    <CheckCircleOutlined style={{ color: "green" }} />
+                  ))
+                }
+                title={item.title}
+                description={
+                  <div>
+                    <p
+                      style={{
+                        margin: "0 auto",
+                        color: (theme === "dark" && "white") || "black",
+                      }}
+                    >
+                      {item.id}
+                    </p>
+                    <p style={{ margin: "0 auto", color: "#b2aeae" }}>
+                      {item.status}
+                    </p>
+                  </div>
+                }
+              />
+            </List.Item>
+          )}
+        />
+      </Card>
     </ConfigProvider>
   );
 }
